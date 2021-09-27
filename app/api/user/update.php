702760f2559d176871e6d96eec758686570ef71e
Martha Maria Nieto Ramirez <marthanietods@nube.unadmexico.mx>
2021-09-26 21:17:19 -0700

    Update: main - added funtionality to update and delete a user with Rest API.

---
+++ app/api/user/update.php
@@ -0,0 +1,36 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+include_once '../../core/Database.php';
+include_once '../../models/User.php';
+
+//Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+//Instatiate User user object
+$user = new User($db);
+
+//Get raw user data
+$data = json_decode(file_get_contents("php://input"));
+
+//Set id to update
+$user->id = $data->id;
+
+$user->name = $data->name;
+
+//Update user
+if ($user->update()) {
+    echo json_encode(
+        array('message' => 'User Updated')
+    );
+} else {
+    echo json_encode(
+        array('message' => 'User Not Updated')
+    );
+}
+
--- app/models/User.php
+++ app/models/User.php
@@ -46,10 +46,54 @@ class User
         $stmt = $this->conn->prepare($query);
 
         //Clean data
-        $this->title = htmlspecialchars(strip_tags($this->name));
+        $this->name = htmlspecialchars(strip_tags($this->name));
 
         //Bind data
         $stmt->bindParam(':name', $this->name);
+
+        if($stmt->execute()) {
+            return true;
+        }
+
+        printf("Error: %s.\n", $stmt->error);
+        return false;
+    }
+
+    //Update User
+    public function update()
+    {
+        //Create query
+        $query = 'UPDATE user SET name = :name WHERE id = :id';
+        //Prepare statement
+        $stmt = $this->conn->prepare($query);
+
+        //Clean data
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        //Bind data
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':id', $this->id);
+
+        if($stmt->execute()) {
+            return true;
+        }
+
+        printf("Error: %s.\n", $stmt->error);
+        return false;
+    }
+
+    //Delete user
+    public function delete()
+    {
+        //Create query
+        $query = 'DELETE FROM user WHERE id = :id';
+        //Prepare statement
+        $stmt = $this->conn->prepare($query);
+        //Clean data
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        //Bind data
+        $stmt->bindParam(':id', $this->id);
 
         if($stmt->execute()) {
             return true;
